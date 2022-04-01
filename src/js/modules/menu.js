5219a131dfcb1f45f8a3170c0a0719e453cba028
xabi1000 <babelwado@gmail.com>
2022-04-01 19:23:25 +0200

    template includes responsive menu, finished

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -1,13 +1,13 @@
+const $toggleMenu = document.getElementById('toggle-menu');
 const $toggleMenuBar = document.querySelectorAll('.toggle-menu > div');
 const $sideBar = document.getElementById('side-bar');
 
-const menu = () =>{
-    document.addEventListener('click', (e) => {
-        if (e.target.closest('#toggle-menu')){
-            $sideBar.classList.toggle('active');
-            $toggleMenuBar.forEach(item => item.classList.toggle(`active`));
-        }
-    })
-
-}
-export {menu}
+const menu = () => {
+	document.addEventListener('click', (e) => {
+		if (e.target.closest('#toggle-menu')) {
+			$toggleMenu.classList.toggle('active');
+			$sideBar.classList.toggle('active');
+		}
+	});
+};
+export { menu };
